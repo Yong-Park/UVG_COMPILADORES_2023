d8f759d0734565525f41c4104e1d712df7de8151
yong park <yong.park123456@gmail.com>
2023-07-12 15:33:59 -0600

    realizando pruebas para entender

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import sys
 from antlr4 import *
+from YAPLLexer import YAPLLexer
+from YAPLParser import YAPLParser
+
+def main(argv):
+    input_stream = FileStream(argv[1])
+    lexer = YAPLLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = YAPLParser(stream)
+    tree = parser.startRule()
+ 
+if __name__ == '__main__':
+    main(sys.argv)
